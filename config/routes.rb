948d673fc622ec7711bbc30f6cdff5f003a54d16
Aleksi Hentila <aleksi.hentila@gmail.com>
2012-06-19 13:11:35 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
 
   root to: 'static_pages#home'
 
@@ -10,6 +9,8 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
   
   match '/signup', to: 'users#new'
+  
+  resources :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
